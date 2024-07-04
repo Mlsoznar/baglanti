6408f354acadc0e641897632df977a239fbdfdb1
Mls.oznar <20060395@stu.omu.edu.tr>
2024-07-04 14:30:51 +0300

    yeni

--- index.js
+++
@@ -1,7 +0,0 @@
-// const { startServer } = require('./server');
-// const { initializeWifi, checkNetwork } = require('./services/wifiService');
-
-// const targetSSID = "STARBUCKS FREE WIFI"; // Replace with your mobile hotspot SSID
-
-// initializeWifi();
-// checkNetwork(targetSSID, startServer);
